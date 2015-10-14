14737c0e052d98fbcee1c9fcc915490f267982e1
Matthias Felleisen <matthias@ccs.neu.edu>
2015-10-14 17:19:14 -0400

    adjusted to new population

---
+++ V2/evolution.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(provide
+ ;; Population N N N -> [Listof Payoff]
+ ;; (evolve p c s r) computes the list of average payoffs over the evolution of population p
+ ;; for c cycles of of match-ups with r rounds per match and at death-birth rate of s
+ evolve)
+
+;; ---------------------------------------------------------------------------------------------------
+(require "automata.rkt" "population.rkt" "utilities.rkt")
+
+(define (evolve population cycles rate rounds)
+  (cond
+    [(zero? cycles) '()]
+    [else (define p2 (match-ups population rounds))
+          (define p* (population-payoffs p2))
+          (cons (relative-average p* (sum p*))
+                (evolve (death-birth p2 rate) (- cycles 1) rate rounds))]))
