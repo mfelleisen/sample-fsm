45a1c95707646212aa98b5a75a36486792a92d5d
Matthias Felleisen <matthias@ccs.neu.edu>
2015-10-18 20:23:44 -0400

    the OO version

---
+++ VOO/README.md
@@ -0,0 +1,9 @@
+A Finite-state Simulation of Social-science Games
+=================================================
+
+This is the object-oriented version of the finite-state simulation of
+interacting automaton. 
+
+My goal is o explore (1) the performance of an object-oriented version
+versus a mostly functional one and (2) the gradual-typing performance
+hypothesis for object-oriented programming. 
---
+++ VOO/fsm0.rkt
@@ -0,0 +1,53 @@
+#! /usr/bin/env racket -tm
+#lang racket
+
+;; Run a Simulation of Interacting Automata
+
+(provide main)
+
+;; =============================================================================
+(require "population.rkt" "utilities.rkt" plot)
+
+(plot-new-window? #t)
+
+;; -> Void
+;; effect: run timed simulation, create and display plot of average payoffs
+;; effect: measure time needed for the simulation
+(define (main)
+  (collect-garbage)
+  (collect-garbage)
+  (collect-garbage)
+  (define ps
+    (time
+     (simulation->lines
+      (evolve (build-random-population 100) 1000 10 20))))
+  (define h3 (function (lambda (x) 3) #:color "blue"))
+  (define h1 (function (lambda (x) 1) #:color "red"))
+  (plot (list h3 h1 ps) #:y-min 0.0 #:y-max 4.0))
+
+;; [Listof Payoff] -> [Listof [List Real Real]]
+;; turn average payoffs into a list of Cartesian points 
+(define (simulation->lines data)
+  (define coors (for/list ([d (in-list data)][n (in-naturals)]) (list n d)))
+  (lines coors))
+
+;; Population N N N -> [Listof Payoff]
+;; computes the list of average payoffs over the evolution of population p for
+;; c cycles of of match-ups with r rounds per match and at birth/death rate of s
+(define (evolve p c s r)
+  (cond
+    [(zero? c) '()]
+    [else (define p2 (send p match-up* r))
+          (define pp (send p2 payoffs))
+          (define p3 (send p2 death-birth s))
+          (cons (relative-average pp r) (evolve p3 (- c 1) s r))]))
+
+
+;; -----------------------------------------------------------------------------
+(module+ five
+  (main)
+  (main)
+  (main)
+  (main)
+  (main))
+
